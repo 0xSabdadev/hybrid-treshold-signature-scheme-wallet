df488f1a958bf5965a58e6084d74264964a0ddb3
0xSabdadev <jasonalhilal@gmail.com>
2024-02-12 11:07:06 +0700

    new

--- contracts/MultiSigFactory.sol
+++ contracts/MultiSigFactory.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./TSSWallet.sol";
+import "./Multisig.sol";
 
 contract MultiSigFactory{
 
--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -4,41 +4,43 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/utils/cryptography/ECDSA.sol";
 import "./MultiSigFactory.sol";
 
 contract MultiSigTA {
-    
-    address mainOwner;
+    using ECDSA for bytes32; // Using ECDSA library for bytes32 type
+
+    address[] public walletOwners;
     address multisigIntance;
-    address[] walletowners;
-    uint limit;  
-    uint depositId = 0;
-    uint withdrawalId = 0;
-    uint transferId = 0;
-    string[] tokenList;
+    address mainOwner;
+    uint public limit;
     bool private locked;
-    
+    uint public depositId = 0;
+    uint public withdrawalId = 0;
+    uint public transferId = 0;
+    string[] public tokenList;
+
     constructor() {
         mainOwner = msg.sender;
-        walletowners.push(mainOwner);
-        limit = walletowners.length - 1;
+        walletOwners.push(mainOwner);
+        limit = walletOwners.length - 1;
         tokenList.push("ETH");
         locked = false;
     }
-    
-    mapping(address => mapping(string => uint)) balance;
-    mapping(address => mapping(uint => bool)) approvals;
-    mapping(address => bool) withdrawing;
+
+    mapping(address => mapping(string => uint)) public balance;
+    mapping(address => mapping(uint => bool)) public approvals;
+    mapping(address => bool) public withdrawing;
     mapping(address => bool) public withdrawalInProcess;
-    mapping(string => Token) tokenMapping;
+    mapping(string => Token) public tokenMapping;
     mapping(address => uint) public balancesToWithdraw;
     mapping(address => string) public tickerToWithdraw;
-    
+
     struct Token {
         string ticker;
         address tokenAddress;
     }
-    
+
     struct Transfer {
         string ticker;
         address sender;
@@ -47,10 +49,11 @@ contract MultiSigTA {
         uint id;
         uint approvals;
         uint timeOfTransaction;
+        bool executed;
     }
-    
-    Transfer[] transferRequests;
-    
+
+    Transfer[] public transferRequests;
+
     event walletOwnerAdded(address addedBy, address ownerAdded, uint timeOfTransaction);
     event walletOwnerRemoved(address removedBy, address ownerRemoved, uint timeOfTransaction);
     event fundsDeposited(string ticker, address sender, uint amount, uint depositId, uint timeOfTransaction);
@@ -60,26 +63,28 @@ contract MultiSigTA {
     event transferApproved(string ticker, address sender, address receiver, uint amount, uint id, uint approvals, uint timeOfTransaction);
     event fundsTransfered(string ticker, address sender, address receiver, uint amount, uint id, uint approvals, uint timeOfTransaction);
     event tokenAdded(address addedBy, string ticker, address tokenAddress, uint timeOfTransaction);
-    
+    mapping(uint => mapping(address => bool)) approvedByTransfer; // Mapping untuk menyimpan data approvedBy
+
     modifier onlyOwners() {
-       bool isOwner = false;
-       for (uint i = 0; i< walletowners.length; i++) {
-           if (walletowners[i] == msg.sender) {
-               isOwner = true;
-               break;
-           }
-       }
-       require(isOwner == true, "hanya wallet owners yang bisa memanggil function ini");
-       _;
+        bool ownerFound = false; // Mengubah nama variabel agar tidak bertabrakan dengan fungsi lokal
+        for (uint i = 0; i < walletOwners.length; i++) {
+            if (walletOwners[i] == msg.sender) {
+                ownerFound = true;
+                break;
+            }
+        }
+        require(ownerFound == true, "Only wallet owners can call this function");
+        _;
     }
-    
+
+
     modifier tokenExists(string memory ticker) {
-        if(keccak256((bytes(ticker))) != keccak256(bytes("ETH"))){
-            require(tokenMapping[ticker].tokenAddress != address(0), "token tidak tersedia");
+        if (keccak256(bytes(ticker)) != keccak256(bytes("ETH"))) {
+            require(tokenMapping[ticker].tokenAddress != address(0), "Token is not available");
         }
         _;
     }
-    
+
     modifier mutexApplied() {
         require(!locked, "Reentrancy guard: locked");
         locked = true;
@@ -87,16 +92,23 @@ contract MultiSigTA {
         locked = false;
     }
 
+    modifier hasNotApproved(uint id) {
+        require(!approvals[msg.sender][id], "Signature has already been collected");
+        _;
+    }
+
+    
+
     function addToken(string memory ticker, address _tokenAddress) public onlyOwners {
         for (uint i = 0; i < tokenList.length; i++) {
-            require(keccak256(bytes(tokenList[i])) != keccak256(bytes(ticker)), "tidak dapat add tokens duplikat");
+            require(keccak256(bytes(tokenList[i])) != keccak256(bytes(ticker)), "Cannot add duplicate token");
         }
-        require(keccak256(bytes(ERC20(_tokenAddress).symbol())) == keccak256(bytes(ticker)),"token tidak tersedia pada ERC20");
+        require(keccak256(bytes(ERC20(_tokenAddress).symbol())) == keccak256(bytes(ticker)), "Token is not available in ERC20");
         tokenMapping[ticker] = Token(ticker, _tokenAddress);
         tokenList.push(ticker);
         emit tokenAdded(msg.sender, ticker, _tokenAddress, block.timestamp);
     }
-        
+
     function setMultisigContractAdress(address walletAddress) private {
         multisigIntance = walletAddress;
     }
@@ -112,101 +124,96 @@ contract MultiSigTA {
     }
 
     function addWalletOwner(address owner, address walletAddress, address _address) public onlyOwners {
-       for (uint i = 0; i < walletowners.length; i++) {
-           if(walletowners[i] == owner) {
-               revert("cannot add duplicate owners");
-           }
-       }
-        walletowners.push(owner);
-        limit = walletowners.length - 1;
+        for (uint i = 0; i < walletOwners.length; i++) {
+            require(walletOwners[i] != owner, "Cannot add duplicate owner");
+        }
+        require(limit < 2, "Limit reached");
+        walletOwners.push(owner);
+        limit = walletOwners.length - 1;
         emit walletOwnerAdded(msg.sender, owner, block.timestamp);
         setMultisigContractAdress(walletAddress);
         callAddOwner(owner, _address);
     }
-    
+
     function removeWalletOwner(address owner, address walletAddress, address _address) public onlyOwners {
         bool hasBeenFound = false;
         uint ownerIndex;
-        for (uint i = 0; i < walletowners.length; i++) {
-            if(walletowners[i] == owner) {
+        for (uint i = 0; i < walletOwners.length; i++) {
+            if (walletOwners[i] == owner) {
                 hasBeenFound = true;
                 ownerIndex = i;
                 break;
             }
         }
-        require(hasBeenFound == true, "wallet owner tidak terdetect");
-        walletowners[ownerIndex] = walletowners[walletowners.length - 1];
-        walletowners.pop();
-        limit = walletowners.length - 1;
+        require(hasBeenFound == true, "Wallet owner not detected");
+        walletOwners[ownerIndex] = walletOwners[walletOwners.length - 1];
+        walletOwners.pop();
+        limit = walletOwners.length - 1;
         emit walletOwnerRemoved(msg.sender, owner, block.timestamp);
         setMultisigContractAdress(walletAddress);
         callRemoveOwner(owner, _address);
     }
-    
-    function deposit(string memory ticker, uint amount) public payable onlyOwners mutexApplied{
-        require(balance[msg.sender][ticker] >= 0, "tidak dapat deposit value 0");
-        require(!withdrawing[msg.sender], "proses sedang berlangsung");
-        withdrawing[msg.sender] = true;
-        if(keccak256(bytes(ticker)) == keccak256(bytes("ETH"))) {
+
+    function deposit(string memory ticker, uint amount) public payable onlyOwners tokenExists(ticker) {
+        require(balance[msg.sender][ticker] >= 0, "Cannot deposit 0 value");
+        if (keccak256(bytes(ticker)) == keccak256(bytes("ETH"))) {
             balance[msg.sender]["ETH"] += msg.value;
-        }else {
-            require(tokenMapping[ticker].tokenAddress != address(0), "token tidak tersedia");
+        } else {
+            require(tokenMapping[ticker].tokenAddress != address(0), "Token is not available");
             bool transferSuccess = IERC20(tokenMapping[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
             require(transferSuccess, "ERC20 transfer failed");
             balance[msg.sender][ticker] += amount;
         }
-        emit fundsDeposited(ticker, msg.sender, msg.value, depositId, block.timestamp);
-        withdrawing[msg.sender] = false;
+        emit fundsDeposited(ticker, msg.sender, amount, depositId, block.timestamp);
         depositId++;
-    } 
-    
-    function initiateWithdrawal(string memory ticker, uint _amount) public onlyOwners {
-        require(_amount > 0, "Amount must be greater than 0");
-        require(balance[msg.sender][ticker] >= _amount,"balance tidak cukup");
-        require(!withdrawalInProcess[msg.sender], "Withdrawal request in process");
-        if(keccak256(bytes(ticker)) != keccak256(bytes("ETH"))) {
-            require(tokenMapping[ticker].tokenAddress != address(0), "token tidak tersedia");
-        }
-        balancesToWithdraw[msg.sender] = _amount;
-        tickerToWithdraw[msg.sender] = ticker;
-        withdrawalInProcess[msg.sender] = true;
     }
 
-    function withdraw() public onlyOwners mutexApplied{
-        require(withdrawalInProcess[msg.sender], "Tidak ada withdrawal request");
-        require(!withdrawing[msg.sender], "proses sedang berlangsung");
-        withdrawing[msg.sender] = true;
+    function withdraw(string memory ticker, uint _amount) public onlyOwners {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(balance[msg.sender][ticker] >= _amount, "Insufficient balance");
+        if (keccak256(bytes(ticker)) != keccak256(bytes("ETH"))) {
+            require(tokenMapping[ticker].tokenAddress != address(0), "Token is not available");
+        }
         bool transferSuccess = false;
-        uint amount = balancesToWithdraw[msg.sender];
-        string memory ticker = tickerToWithdraw[msg.sender];
-        balancesToWithdraw[msg.sender] = 0;
-        tickerToWithdraw[msg.sender] = "";
-        withdrawalInProcess[msg.sender] = false;
-        if(keccak256(bytes(ticker)) == keccak256(bytes("ETH"))) {
-            // payable(msg.sender).transfer(amount);
-            (transferSuccess,) = payable(msg.sender).call{value: amount}("");
-        }else {
-            require(tokenMapping[ticker].tokenAddress != address(0), "token tidak tersedia");
-            transferSuccess = IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, amount);
+        if (keccak256(bytes(ticker)) == keccak256(bytes("ETH"))) {
+            (transferSuccess,) = payable(msg.sender).call{value: _amount}("");
+        } else {
+            require(tokenMapping[ticker].tokenAddress != address(0), "Token is not available");
+            transferSuccess = IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, _amount);
         }
         require(transferSuccess, "ERC20 transfer failed");
-        balance[msg.sender][ticker] -= amount;
-        emit fundsWithdrawed(ticker, msg.sender, amount, withdrawalId, block.timestamp);
-        withdrawing[msg.sender] = false;
+        balance[msg.sender][ticker] -= _amount;
+        emit fundsWithdrawed(ticker, msg.sender, _amount, withdrawalId, block.timestamp);
         withdrawalId++;
     }
-    // NOTE : TokenExiz Hndle
-    function createTransferRequest(string memory ticker, address payable receiver, uint amount) public onlyOwners tokenExists(ticker){
-        require(balance[msg.sender][ticker] >= amount, "funds tidak cukup untuk create transfer");
-        for (uint i = 0; i < walletowners.length; i++) {
-            require(walletowners[i] != receiver, "tidak bisa transfer funds pada wallet pribadi");
+
+    function createTransferRequest(string memory ticker, address payable receiver, uint amount, bytes memory signature) public onlyOwners tokenExists(ticker) {
+        require(balance[msg.sender][ticker] >= amount, "Insufficient balance for transfer");
+        require(walletOwners.length == 3, "TSS requires exactly 3 owners");
+        require(receiver != address(0), "Invalid receiver address");
+        for (uint i = 0; i < walletOwners.length; i++) {
+            require(walletOwners[i] != receiver, "tidak bisa transfer funds pada wallet pribadi");
         }
+        // Verify signature of the sender
+        bytes32 messageHash = keccak256(abi.encodePacked(ticker, receiver, amount));
+
+        // Concatenate signature and message hash for verification
+        bytes32 prefixedHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        address signer = prefixedHash.recover(signature);
+        require(isOwner(signer), "Invalid signature");
+
+        // Mark the sender as approved
+        approvedByTransfer[transferId][msg.sender] = true; // Update mapping approvedBy
+        
+        
         balance[msg.sender][ticker] -= amount;
-        transferRequests.push(Transfer(ticker, msg.sender, receiver, amount, transferId, 0, block.timestamp));
+        transferRequests.push(Transfer(ticker, msg.sender, receiver, amount, transferId, 1, block.timestamp, false));
         transferId++;
-        emit transferCreated(ticker, msg.sender, receiver, amount, transferId, 0, block.timestamp);
+
+        // Emit event for transfer request
+        emit transferCreated(ticker, msg.sender, receiver, amount, transferId, 1, block.timestamp);
     }
-    
+
     function cancelTransferRequest( string memory ticker, uint id) public onlyOwners {
         // string memory ticker = transferRequests[id].ticker;
         bool hasBeenFound = false;
@@ -227,35 +234,38 @@ contract MultiSigTA {
         emit transferCancelled(ticker, msg.sender, transferRequests[transferIndex].receiver, transferRequests[transferIndex].amount, transferRequests[transferIndex].id, transferRequests[transferIndex].approvals, transferRequests[transferIndex].timeOfTransaction);
         transferRequests.pop();
     }
-    
-    function approveTransferRequest( string memory ticker, uint id) public onlyOwners {
-        // string memory ticker = transferRequests[id].ticker;
-        bool hasBeenFound = false;
-        uint transferIndex = 0;
-        for (uint i = 0; i < transferRequests.length; i++) {
-            if(transferRequests[i].id == id) {
-                hasBeenFound = true;
-                break;
-            }
-             transferIndex++;
-        }
-        
-        require(hasBeenFound, "hanya transfer creator yang dapat cancel");
-        require(approvals[msg.sender][id] == false, "tidak dapat approve pada transaksi transfer kedua kalinya");
-        require(transferRequests[transferIndex].sender != msg.sender, "tidak dapat approve pada transaction pribadi");
-        
-        approvals[msg.sender][id] = true;
-        transferRequests[transferIndex].approvals++;
-        
-        emit transferApproved(ticker, msg.sender, transferRequests[transferIndex].receiver, transferRequests[transferIndex].amount, transferRequests[transferIndex].id, transferRequests[transferIndex].approvals, transferRequests[transferIndex].timeOfTransaction);
-        if (transferRequests[transferIndex].approvals == limit) {
-            transferFunds(ticker, transferIndex);
+
+    function approveTransferRequest(string memory ticker,uint id, bytes memory signature) public onlyOwners {
+        require(id < transferRequests.length, "Invalid transfer ID");
+        require(!transferRequests[id].executed, "Transfer request already executed");
+
+        // Verify signature of the sender
+        bytes32 messageHash = keccak256(abi.encodePacked(id));
+        bytes32 prefixedHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        address signer = prefixedHash.recover(signature);
+        require(isOwner(signer), "Invalid signature");
+
+        // Check if the sender has not approved before
+        require(!approvedByTransfer[id][signer], "Duplicate signature");
+
+        // Mark the sender as approved
+        approvedByTransfer[id][signer] = true;
+        transferRequests[id].approvals++;
+
+        // Check if enough valid signatures were provided
+        require(transferRequests[id].approvals >= limit, "Insufficient approvals");
+
+        // Execute the transfer
+        transferRequests[id].receiver.transfer(transferRequests[id].amount);
+        transferRequests[id].executed = true;
+        emit transferApproved(ticker, msg.sender, transferRequests[id].receiver, transferRequests[id].amount, id, transferRequests[id].approvals, block.timestamp);
+        // emit transferApproved(ticker, msg.sender, transferRequests[transferIndex].receiver, transferRequests[transferIndex].amount, transferRequests[transferIndex].id, transferRequests[transferIndex].approvals, transferRequests[transferIndex].timeOfTransaction);
+        if (transferRequests[id].approvals == limit) {
+            transferFunds(ticker, id);
         }
     }
-    
+
     function transferFunds(string memory ticker, uint id) private mutexApplied{
-        require(!withdrawing[msg.sender], "proses sedang berlangsung");
-        withdrawing[msg.sender] = true;
         bool transferSuccess = false;
         balance[transferRequests[id].receiver][ticker] += transferRequests[id].amount;
         if(keccak256(bytes(ticker)) == keccak256(bytes("ETH"))) {
@@ -267,37 +277,45 @@ contract MultiSigTA {
         }
         require(transferSuccess, "ERC20 transfer failed");
         emit fundsTransfered(ticker, msg.sender, transferRequests[id].receiver, transferRequests[id].amount, transferRequests[id].id, transferRequests[id].approvals, transferRequests[id].timeOfTransaction);
-        withdrawing[msg.sender] = false;
         transferRequests[id] = transferRequests[transferRequests.length - 1];
         transferRequests.pop();
     }
-    
-    function getApprovals(uint id) public onlyOwners view returns(bool) {
+
+
+    function getApprovals(uint id) public onlyOwners view returns (bool) {
         return approvals[msg.sender][id];
     }
-    
-    function getTransferRequests() public onlyOwners view returns(Transfer[] memory) {
+
+    function isOwner(address _address) private view returns (bool) {
+        for (uint i = 0; i < walletOwners.length; i++) {
+            if (walletOwners[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getTransferRequests() public onlyOwners view returns (Transfer[] memory) {
         return transferRequests;
     }
-    
-    function getBalance(string memory ticker) public view returns(uint) {
+
+    function getBalance(string memory ticker) public view returns (uint) {
         return balance[msg.sender][ticker];
     }
-    
+
     function getApprovalLimit() public onlyOwners view returns (uint) {
         return limit;
     }
-    
-     function getContractBalance() public view returns(uint) {
+
+    function getContractBalance() public view returns (uint) {
         return address(this).balance;
     }
-    
-    function getWalletOwners() public view onlyOwners returns(address[] memory) {
-        return walletowners;
+
+    function getWalletOwners() public view onlyOwners returns (address[] memory) {
+        return walletOwners;
     }
-   
-    function getTokenList() public view returns(string[] memory){
+
+    function getTokenList() public view returns (string[] memory) {
         return tokenList;
     }
-    
 }
